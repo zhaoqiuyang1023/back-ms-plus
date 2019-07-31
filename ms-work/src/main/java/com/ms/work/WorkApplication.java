750af3c1c6fa90a5dec37b5fd38774d4924b59f2
3508238911@qq.com <3508238911@qq.com>
2019-07-31 10:20:07 +0800

    修改后台管理名称

---
+++ ms-work/src/main/java/com/ms/work/WorkApplication.java
@@ -0,0 +1,15 @@
+package com.ms.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Alan
+ * @date : 2019/7/30  15:09
+ */
+@SpringBootApplication
+public class WorkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkApplication.class, args);
+    }
+}
